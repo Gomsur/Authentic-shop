8dba117dbdea430c9bfe8ac6e8cb12d2fec02ed0
Gomsur <robert@eddiebengtsson.se>
2024-04-18 16:25:13 +0000

    Forms are now functional for user, email, password 1 and password 2

--- accounts/forms.py
+++ accounts/forms.py
@@ -4,3 +4,13 @@ from .models import User, Profile
 from django.contrib.auth.forms import UserCreationForm
 
 
+class ProfileForm(ModelForm):
+    class Meta:
+        model = Profile
+        exclude = ('user',)
+
+
+class SignUpForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ['email', 'password1', 'password2',]
