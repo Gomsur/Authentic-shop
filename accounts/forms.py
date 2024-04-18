1d57bf4238b0d21949fb7c9feef88fe3dd4cc4c8
Gomsur <robert@eddiebengtsson.se>
2024-04-18 16:24:50 +0000

    forms continued work

--- accounts/forms.py
+++ accounts/forms.py
@@ -4,13 +4,3 @@ from .models import User, Profile
 from django.contrib.auth.forms import UserCreationForm
 
 
-class ProfileForm(ModelForm):
-    class Meta:
-        model = Profile
-        exclude = ('user',)
-
-
-class SignUpForm(UserCreationForm):
-    class Meta:
-        model = User
-        fields = ['email', 'password1', 'password2',]
