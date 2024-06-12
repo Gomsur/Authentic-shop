4dd1b69b8bc4d8e235ec9601092ad6fa0a572c2d
Gomsur <112901440+Gomsur@users.noreply.github.com>
2024-06-12 08:44:47 +0200

    Validation of forms

--- newsletter/forms.py
+++ newsletter/forms.py
@@ -1,13 +1,45 @@
+from django.forms import ModelForm
+from .models import User, Profile
 from django import forms
-from .models import Subscriber
 
-class SubscriberForm(forms.ModelForm):
+from django.contrib.auth.forms import UserCreationForm
+
+
+class ProfileForm(ModelForm):
     class Meta:
-        model = Subscriber
-        fields = ['email']
+        model = Profile
+        exclude = ('user',)
 
-    def clean_email(self):
-        email = self.cleaned_data.get('email')
-        if Subscriber.objects.filter(email=email).exists():
-            raise forms.ValidationError("This email is already subscribed.")
-        return email
+
+
+
+class SignUpForm(UserCreationForm):
+    password1 = forms.CharField(
+        label="Password",
+        widget=forms.PasswordInput(attrs={
+            'autocomplete': 'new-password',
+            'aria-describedby': 'id_password1_helptext',
+            'class': 'form-control'
+        }),
+        help_text=(
+            '<ul>'
+            '<li>Your password can’t be too similar to your other personal information.</li>'
+            '<li>Your password must contain at least 8 characters.</li>'
+            '<li>Your password can’t be a commonly used password.</li>'
+            '<li>Your password can’t be entirely numeric.</li>'
+            '</ul>'
+        )
+    )
+    password2 = forms.CharField(
+        label="Password confirmation",
+        widget=forms.PasswordInput(attrs={
+            'autocomplete': 'new-password',
+            'aria-describedby': 'id_password2_helptext',
+            'class': 'form-control'
+        }),
+        help_text='<ul><li>Enter the same password as before, for verification.</li></ul>'
+    )
+
+    class Meta:
+        model = User
+        fields = ['email', 'password1', 'password2']
